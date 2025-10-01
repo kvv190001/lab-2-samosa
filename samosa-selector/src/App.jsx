042349682e4d35254744a7320bb453a006ff1617
Viet Khue Vu <kvv190001@users.noreply.github.com>
2025-10-01 15:52:32 -0500

    create the UI

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -4,7 +4,11 @@ const App = () => {
 
   return (
     <div className="App">
-
+      <div className='header'>
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img className='samosa' src='https://www.samosa-recipe.com/wp-content/uploads/2019/01/aloo-samosa.jpg'></img>
+      </div>
     </div>
   )
 }
